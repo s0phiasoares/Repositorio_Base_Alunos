595a32bc37f588acc69b83f37d0d46205e2d9e67
sophia <sophia28.soares@gmail.com>
2025-09-09 08:35:39 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/função.py
@@ -0,0 +1,17 @@
+# declaração de funcao
+def escreva () :
+    print ("oi, tudo bem ?")
+    print ("qual é seu nome?")
+
+#chamada de funcao
+escreva() 
+
+
+#funções diferentes
+
+#declaração de funcao com argumento
+def soma (n1, n2,n3) : 
+    print (n1 * n2 /n3)
+
+soma (5,20,5)
+soma (30,2,4)
