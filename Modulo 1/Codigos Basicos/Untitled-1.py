91fe57bd09d895dd31bfa165e49175100f440b34
sophia <sophia28.soares@gmail.com>
2025-09-09 08:30:43 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/Untitled-1.py
@@ -0,0 +1,19 @@
+valor_original= float (input("digite o valor original: "))
+
+print ("digite o valor original :")
+
+
+
+if valor_original >200:
+    print ("valor original:")
+    print ("porcentagem de desconto: 15%")
+    print ("valor com desconto : (valor total* 0.85)")
+
+elif valor_original >=100:
+      print ("valor original:")
+      print ("porcentagem de desconto: 10%")
+      print ("valor com desconto: (valor total * 0.9)")
+
+else
+     print ("valor original:")
+     print ("essa compra não tem desconto")
