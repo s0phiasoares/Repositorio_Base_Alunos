cd4632d1fbdf3bf541a8ac1c1352023ae50c9c07
sophia <sophia28.soares@gmail.com>
2025-09-09 08:34:01 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/.py
@@ -0,0 +1,14 @@
+
+peso =float (input("digite o peso: "))
+altura =float( input ("digite a altura:"))
+
+print ("peso da pessoa: ", peso)
+print ("altura da pessoa: ", altura)
+
+imc = peso/(altura*altura)
+if imc >= 30:
+   print ("cudado com a saude") 
+
+else : 
+   print ("tudo ok")
+   
