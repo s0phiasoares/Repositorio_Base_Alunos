607b308d171259a498e91593aaf3c67a4153bc24
sophia <sophia28.soares@gmail.com>
2025-09-11 08:48:13 -0300

    Add files via upload

---
+++ Modulo 1/Exercicios/lista.py
@@ -0,0 +1,41 @@
+#exemplo lista : # numeros = [1,2,3,4,5]
+#nomes = ["Joaquim", "Maria" ,"Ana"]
+
+#print(numeros)
+#print(nomes)
+
+frutas = ["Morango", "Uva verde", "Manga", "Banana", "Amora"]
+
+print (frutas [0])
+print (frutas [1])
+print (frutas [2])
+print (frutas [3])
+print (frutas [4])
+
+print("-" * 50)
+
+frutas [3] = "kiwi"
+frutas [0] = "Pessego"
+
+print (frutas [0])
+print (frutas [1])
+print (frutas [2])
+print (frutas [3])
+print (frutas  [4])
+
+
+
+frutas.append("laranja")
+print(frutas [5])
+
+frutas.append("Abacaxi")
+print(frutas [6])
+
+frutas.insert(0,"Maçã")
+print(frutas )
+
+#for fruta in frutas:
+    #print (frutas)
+
+#frutas.pop()
+#print(frutas)
